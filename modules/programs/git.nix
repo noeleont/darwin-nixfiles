e1e9ec3f6079fcc18471d519ea3e55d4e5332c53
Noe Thalheim <noe@bespinian.io>
2025-05-14 16:57:40 +0200

    chore: add git alias

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -25,5 +25,9 @@
       gpgSign = true;
     };
   };
+  aliases = {
+    co = "checkout";
+    lgb = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%n' --abbrev-commit --date=relative --branches";
+  };
   lfs.enable = true;
 }
